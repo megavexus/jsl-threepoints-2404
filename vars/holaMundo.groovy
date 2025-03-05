064e151a2b0cf869c1bf82c9c8752130468aac3e
megavexus <nexus.megavexus@gmail.com>
2025-03-05 18:56:25 +0100

    Update holaMundo.groovy

--- vars/holaMundo.groovy
+++ vars/holaMundo.groovy
@@ -1,7 +1,8 @@
 def call(String name = 'User') {
     echo "Goodbye, ${name}."
+    slackSend channel: 'Proyecto Fenix', message: "Goodbye, ${name}."
 }
 
-def informacion() {
-    echo "Informacion de la libreria"
+def informacion(String name = 'User') {
+    echo "Informacion de la libreria de ${name}"
 }
